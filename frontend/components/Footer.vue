68ae742ec0366ee431e37fc440d7d84adf540891
Youri Novikov <73738882+0youri@users.noreply.github.com>
2024-12-07 12:02:17 +0100

    init

---
+++ frontend/components/Footer.vue
@@ -0,0 +1,20 @@
+<template>
+    <footer class="flex justify-center bg-primary-light text-dark py-5 gap-2 items-center text-lg">
+      <NuxtLink to="https://linktr.ee/0youri" class="cursor-pointer" target="_blank">
+        @0youri
+      </NuxtLink>
+      <span>•</span>
+      <NuxtLink to="https://github.com/0youri/flick-pick" class="cursor-pointer" target="_blank">
+        github repo
+      </NuxtLink>
+      
+    </footer>
+  </template>
+  
+  <script lang="ts" setup>
+  
+  </script>
+  
+  <style>
+  
+  </style>
